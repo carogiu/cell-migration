237f72d0fa5e357fdd1b7832c524adc6a8991966
carogiu <57912591+carogiu@users.noreply.github.com>
2019-12-28 16:51:00 +0100

    UPDATE - Correct model param class

--- model/model_parameter_class.py
+++ model/model_parameter_class.py
@@ -15,57 +15,20 @@ class ModelParam:
 
     def __init__(self, nx, ny, n, theta, epsilon, lmbda, dt, M, vi,
                  velocity=Expression(("10* sin(x[1]) + 5 * x[0] * x[0]", "0.0"), degree=2)):
+        # geometry + rep
         self.nx = nx
         self.ny = ny
         self.n = n
+        # dimensionless parameters DON'T CHANGE
         self.factor = Constant(3 / (2 * np.sqrt(2)))
         self.mid = Constant(0.5)
+        # dimensionless parameters (can change)
         self.theta = Constant(theta)
-        self.epsilon = Constant(epsilon)
+        self.epsilon = Constant(epsilon) # !!! CHANGE EPSILON IN INITIAL CONDITION AS WELL
         self.lmbda = Constant(lmbda)
         self.dt = Constant(dt)
         self.M = Constant(M)
         self.vi = vi
+        # test phase with a velocity
         self. velocity = Expression(velocity, degree=2)
-
-
-
-
-def create_menu(config: MenuConfig):
-    title = config.title
-    body = config.body
-    # ...
-
-
-config = MenuConfig
-config.title = "My delicious menu"
-config.body = "A description of the various items on the menu"
-config.button_text = "Order now!"
-# The instance attribute overrides the default class attribute.
-config.cancellable = True
-
-create_menu(config)
-
-# geometry + rep
-nx = ny = 50
-n = 30
-
-# dimensionless parameters DON'T CHANGE
-factor = 3 / (2 * np.sqrt(2))
-mid = .5
-
-# dimensionless parameters (can change)
-theta = 2
-epsilon = .2  # !!! CHANGE EPSILON IN INITIAL CONDITION AS WELL
-lmbda = 1.0e-02
-dt = 5.0e-06
-M=1
-#vi = "10*sin(x[1]*2*pi)"
-vi ="1"
-
-# test phase with a velocity
-velocity = Expression(("10* sin(x[1]) + 5 * x[0] * x[0]", "0.0"), degree=2)
-
-# set as Dolfin Constants
-
 
