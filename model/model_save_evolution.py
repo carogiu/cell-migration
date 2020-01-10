ee7e950279b23e1241f76c5840543a31b49307ac
carogiu <57912591+carogiu@users.noreply.github.com>
2020-01-10 18:33:27 +0000

    UPDATE - Structure change

--- model/model_save_evolution.py
+++ model/model_save_evolution.py
@@ -26,28 +26,6 @@ def main_save(phi_tot, vx_tot, vy_tot, p_tot, u, U_flow, i, mesh, nx, ny):
 
 
 ### Partial saves
-
-#### Flow
-def array_exp_flow(U_flow, mesh, nx, ny):
-    """
-    From the function U_flow, extract ux, uy and p and return them as (nx x ny) arrays. Need mesh to know the dimensions
-    :param mesh: dolfin mesh
-    :param U_flow: dolfin Function
-    :param nx: int, grid dimension
-    :param ny: int, grid dimension
-    :return: arrays, contain the values of vx, vy and p for a given time
-    """
-    velocity, pressure = U_flow.split()
-    arr_p = pressure.compute_vertex_values(mesh)
-    arr_p = np.reshape(arr_p, (nx + 1, ny + 1))[::-1]
-    arr_u = velocity.compute_vertex_values(mesh)
-    arr_u = np.reshape(arr_u, (2, nx + 1, ny + 1))
-    arr_ux = arr_u[0][::-1]
-    arr_uy = arr_u[1][::-1]
-
-    return arr_ux, arr_uy, arr_p
-
-
 def save_flow(vx_tot, vy_tot, p_tot, U_flow, i, mesh, nx, ny):
     """
     For a given U_flow for time i, extract the corresponding ux, uy and p and saves them at the position i in the
@@ -70,7 +48,44 @@ def save_flow(vx_tot, vy_tot, p_tot, U_flow, i, mesh, nx, ny):
     return vx_tot, vy_tot, p_tot
 
 
-#### Phase
+def save_phi(phi_tot, u, i, mesh, nx, ny):
+    """
+    For a given u for time i, extract the phase and saves it at position i in phi_tot
+    @param phi_tot: array, contains all the values of phi for all the intermediate times
+    @param u: dolfin Function
+    @param i: int, number of the time step
+    @param mesh: dolphin mesh
+    @param nx: int, grid dimension
+    @param ny: int, grid dimension
+    @return: array
+    """
+    arr_phi, _ = array_exp_phase(u, mesh, nx, ny)
+    phi_tot[:, :, i] = arr_phi
+
+    return phi_tot
+
+
+#### Dolfin function to explicit array
+def array_exp_flow(U_flow, mesh, nx, ny):
+    """
+    From the function U_flow, extract ux, uy and p and return them as (nx x ny) arrays. Need mesh to know the dimensions
+    :param mesh: dolfin mesh
+    :param U_flow: dolfin Function
+    :param nx: int, grid dimension
+    :param ny: int, grid dimension
+    :return: arrays, contain the values of vx, vy and p for a given time
+    """
+    velocity, pressure = U_flow.split()
+    arr_p = pressure.compute_vertex_values(mesh)
+    arr_p = np.reshape(arr_p, (nx + 1, ny + 1))[::-1]
+    arr_u = velocity.compute_vertex_values(mesh)
+    arr_u = np.reshape(arr_u, (2, nx + 1, ny + 1))
+    arr_ux = arr_u[0][::-1]
+    arr_uy = arr_u[1][::-1]
+
+    return arr_ux, arr_uy, arr_p
+
+
 def array_exp_phase(u, mesh, nx, ny):
     """
     From the function u, extracts the phase phi and the chemical potential mu and saves them as arrays
@@ -89,20 +104,3 @@ def array_exp_phase(u, mesh, nx, ny):
     arr_mu = np.reshape(arr_mu, (nx + 1, ny + 1))[::-1]
 
     return arr_phi, arr_mu
-
-
-def save_phi(phi_tot, u, i, mesh, nx, ny):
-    """
-    For a given u for time i, extract the phase and saves it at position i in phi_tot
-    @param phi_tot: array, contains all the values of phi for all the intermediate times
-    @param u: dolfin Function
-    @param i: int, number of the time step
-    @param mesh: dolphin mesh
-    @param nx: int, grid dimension
-    @param ny: int, grid dimension
-    @return: array
-    """
-    arr_phi, _ = array_exp_phase(u, mesh, nx, ny)
-    phi_tot[:, :, i] = arr_phi
-
-    return phi_tot
