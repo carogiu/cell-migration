5d48f11fa2bfc3135beb23f85398bb569b98b483
carogiu <57912591+carogiu@users.noreply.github.com>
2020-01-10 18:49:17 +0000

    UPDATE - Renaming variables

--- model/model_flow.py
+++ model/model_flow.py
@@ -1,5 +1,7 @@
 ### Packages
-from dolfin import *
+# from dolfin import *
+import dolfin
+from ufl import dx, dot, div, inner, grad
 
 
 ### Main functions
@@ -11,17 +13,17 @@ def space_flow(mesh):
     :param mesh: mesh
     :return: Function space
     """
-    P2 = VectorElement("Lagrange", mesh.ufl_cell(), 2)
-    P1 = FiniteElement("Lagrange", mesh.ufl_cell(), 1)
-    TH = P2 * P1
-    W_flow = FunctionSpace(mesh, TH)
-    return W_flow
+    element_P2 = dolfin.VectorElement("Lagrange", mesh.ufl_cell(), 2)
+    element_P1 = dolfin.FiniteElement("Lagrange", mesh.ufl_cell(), 1)
+    mixed_TH = element_P2 * element_P1
+    w_flow = dolfin.FunctionSpace(mesh, mixed_TH)
+    return w_flow
 
 
-def problem_coupled(W_flow, phi, mu, vi, theta, factor, epsilon):
+def problem_coupled(w_flow, phi, mu, vi, theta, factor, epsilon):
     """
     Solves the phase field problem, with the coupling, with inflow, no growth, no activity
-    @param W_flow: Function space
+    @param w_flow: Function space
     @param phi: Function, phase
     @param mu: Function, chemical potential
     @param vi: Expression, inflow
@@ -30,34 +32,36 @@ def problem_coupled(W_flow, phi, mu, vi, theta, factor, epsilon):
     @param epsilon: float, length scale ratio
     @return:
     """
-    bcs = boundary_conditions_flow(W_flow, vi)
+    bcs = boundary_conditions_flow(w_flow, vi)
     theta_p = theta_phi(theta, phi)
-    (velocity, pressure) = TrialFunctions(W_flow)
-    (v_test, p_test) = TestFunctions(W_flow)
+    (velocity, pressure) = dolfin.TrialFunctions(w_flow)
+    (v_test, p_test) = dolfin.TestFunctions(w_flow)
     a_flow = theta_p * dot(velocity, v_test) * dx - pressure * div(v_test) * dx + p_test * div(
         velocity) * dx
     L_flow = (factor / epsilon) * div(v_test) * phi * mu * dx
-    U_flow = Function(W_flow)
-    solve(a_flow == L_flow, U_flow, bcs=bcs, solver_parameters={"linear_solver": "lu"},
-          form_compiler_parameters={"optimize": True})
-    return U_flow
+    u_flow = dolfin.Function(w_flow)
+    dolfin.solve(a_flow == L_flow, u_flow, bcs=bcs, solver_parameters={"linear_solver": "lu"},
+                 form_compiler_parameters={"optimize": True})
+    return u_flow
 
 
 ### CREATE BOUNDARIES
-def boundary_conditions_flow(W_flow, vi):
+def boundary_conditions_flow(w_flow, vi):
     """
     Creates the boundary conditions  : no slip condition, velocity inflow, pressure out
-    :param W_flow: Function space
+    :param w_flow: Function space
+    :param vi: Expression, velocity inflow
     :return: array of boundary conditions
     """
-    noslip = Constant((0.0, 0.0))
-    bc0 = DirichletBC(W_flow.sub(0), noslip, top_bottom)
-    inflow = Expression((vi, "0.0"), degree=2)
-    bc1 = DirichletBC(W_flow.sub(0), inflow, left)
-    pressure_out = Constant(0.0)
-    bc2 = DirichletBC(W_flow.sub(1), pressure_out, right)
-    #bcs = [bc0, bc1, bc2]
-    # without the no slip condition
+    no_slip = dolfin.Constant((0.0, 0.0))
+    bc0 = dolfin.DirichletBC(w_flow.sub(0), no_slip, top_bottom)
+    inflow = dolfin.Expression((vi, "0.0"), degree=2)
+    bc1 = dolfin.DirichletBC(w_flow.sub(0), inflow, left)
+    pressure_out = dolfin.Constant(0.0)
+    bc2 = dolfin.DirichletBC(w_flow.sub(1), pressure_out, right)
+    # with the no-slip condition
+    # bcs = [bc0, bc1, bc2]
+    # without the no-slip condition
     bcs = [bc1, bc2]
     return bcs
 
@@ -72,7 +76,7 @@ def right(x, on_boundary):
     @param on_boundary: dolfin parameter
     @return: boolean
     """
-    return x[0] > (1.0 - DOLFIN_EPS)
+    return x[0] > (1.0 - dolfin.DOLFIN_EPS)
 
 
 def left(x, on_boundary):
@@ -82,7 +86,7 @@ def left(x, on_boundary):
     @param on_boundary: dolfin parameter
     @return: boolean
     """
-    return x[0] < DOLFIN_EPS
+    return x[0] < dolfin.DOLFIN_EPS
 
 
 def top_bottom(x, on_boundary):
@@ -92,7 +96,7 @@ def top_bottom(x, on_boundary):
     @param on_boundary: dolfin parameter
     @return: boolean
     """
-    return x[1] > 1.0 - DOLFIN_EPS or x[1] < DOLFIN_EPS
+    return x[1] > 1.0 - dolfin.DOLFIN_EPS or x[1] < dolfin.DOLFIN_EPS
 
 
 ### Transformation
@@ -108,42 +112,42 @@ def theta_phi(theta, phi):
 
 
 ### TESTS Functions
-def problem_stokes_flow(W_flow, vi):  # THIS IS WORKING
+def problem_stokes_flow(w_flow, vi):  # THIS IS WORKING
     """
     Creates the function U for the problem of Stokes flow (U = u, p)
-    :param W_flow: Function space
+    :param w_flow: Function space
     :param vi: Expression, inflow velocity
     :return: dolfin Function
     """
-    bcs = boundary_conditions_flow(W_flow, vi)
-    (velocity, pressure) = TrialFunctions(W_flow)
-    (v_test, p_test) = TestFunctions(W_flow)
-    f = Constant((0.0, 0.0))
+    bcs = boundary_conditions_flow(w_flow, vi)
+    (velocity, pressure) = dolfin.TrialFunctions(w_flow)
+    (v_test, p_test) = dolfin.TestFunctions(w_flow)
+    f = dolfin.Constant((0.0, 0.0))
     a_flow = inner(grad(velocity), grad(v_test)) * dx + div(v_test) * pressure * dx + p_test * div(velocity) * dx
     L_flow = inner(f, v_test) * dx
-    U_flow = Function(W_flow)
-    solve(a_flow == L_flow, U_flow, bcs=bcs, solver_parameters={"linear_solver": "lu"},
-          form_compiler_parameters={"optimize": True})
+    U_flow = dolfin.Function(w_flow)
+    dolfin.solve(a_flow == L_flow, U_flow, bcs=bcs, solver_parameters={"linear_solver": "lu"},
+                 form_compiler_parameters={"optimize": True})
     return U_flow
 
 
-def flow_static_phase_no_mu(W_flow, vi, phi, theta):  # THIS IS WORKING
+def flow_static_phase_no_mu(w_flow, vi, phi, theta):  # THIS IS WORKING
     """
     Partially solves the phase field with inflow vi, but no coupling term
-    @param W_flow: Function space
+    @param w_flow: Function space
     @param vi: Expression, inflow velocity
     @param phi: Function, phase
     @param theta: float, friction ratio
     @return:
     """
     theta_p = theta_phi(theta, phi)
-    bcs = boundary_conditions_flow(W_flow, vi)
-    (velocity, pressure) = TrialFunctions(W_flow)
-    (v_test, p_test) = TestFunctions(W_flow)
-    f = Constant(0.0)
+    bcs = boundary_conditions_flow(w_flow, vi)
+    (velocity, pressure) = dolfin.TrialFunctions(w_flow)
+    (v_test, p_test) = dolfin.TestFunctions(w_flow)
+    f = dolfin.Constant(0.0)
     a_flow = theta_p * dot(velocity, v_test) * dx - pressure * div(v_test) * dx + p_test * div(velocity) * dx
     L_flow = f * p_test * dx
-    U_flow = Function(W_flow)
-    solve(a_flow == L_flow, U_flow, bcs=bcs, solver_parameters={"linear_solver": "lu"},
-          form_compiler_parameters={"optimize": True})
-    return U_flow
+    u_flow = dolfin.Function(w_flow)
+    dolfin.solve(a_flow == L_flow, u_flow, bcs=bcs, solver_parameters={"linear_solver": "lu"},
+                 form_compiler_parameters={"optimize": True})
+    return u_flow
