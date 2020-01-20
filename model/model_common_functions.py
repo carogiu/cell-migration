2d72c882f34a96e95b0818e0b4894e9aeebd5914
carogiu <57912591+carogiu@users.noreply.github.com>
2020-01-20 16:53:53 +0000

    ADD - new Python file for common functions Boundaries are now here Change the tolerance for boundaries from DOLFIN_EPS (1E-16) to 1E-12

---
+++ model/model_common_functions.py
@@ -0,0 +1,44 @@
+import dolfin
+
+# boundaries
+
+class BD_right(dolfin.SubDomain):
+    """
+    :param dim_x: dimension in the direction of x
+    """
+
+    def __init__(self, dim_x, **kwargs):
+        self.dim_x = dim_x
+        super().__init__(**kwargs)
+
+    def inside(self, x, on_boundary):
+        d_x = self.dim_x
+        return on_boundary and dolfin.near(x[0], d_x/2, 1E-12)
+
+
+class BD_left(dolfin.SubDomain):
+    """
+    :param dim_x: dimension in the direction of x
+    """
+
+    def __init__(self, dim_x, **kwargs):
+        self.dim_x = dim_x
+        super().__init__(**kwargs)
+
+    def inside(self, x, on_boundary):
+        d_x = self.dim_x
+        return on_boundary and dolfin.near(x[0], -d_x/2, 1E-12)
+
+
+class BD_top_bottom(dolfin.SubDomain):
+    """
+    :param dim_y: dimension in the direction of y
+    """
+
+    def __init__(self, dim_y, **kwargs):
+        self.dim_y = dim_y
+        super().__init__(**kwargs)
+
+    def inside(self, x, on_boundary):
+        d_y = self.dim_y
+        return on_boundary and (dolfin.near(x[1], 0, 1E-12) or dolfin.near(x[1], d_y, 1E-12))
