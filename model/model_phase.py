3c99fbcc092cecf35990e5eddd0e7ba01edd9b76
carogiu <57912591+carogiu@users.noreply.github.com>
2020-02-01 13:13:01 +0000

    UPDATE - Domain and boundaries are now defined in model_domains New model : slip condition, Cahn and Peclet numbers, new boundary conditions for mu New definition of the initial perturbation

--- model/model_phase.py
+++ model/model_phase.py
@@ -1,8 +1,8 @@
 import random
 import dolfin
 import numpy as np
-from ufl import dx, dot, grad
-from model.model_common_functions import BD_left, BD_right
+from ufl import dot, grad
+from model.model_domains import dom_and_bound
 
 
 ### Initialise the phase
@@ -11,66 +11,54 @@ class InitialConditions(dolfin.UserExpression):  # result is a dolfin Expression
     Creates the initial condition for the phase
     """
 
-    def __init__(self, epsilon, **kwargs):
+    def __init__(self, Cahn, h_0, wave, **kwargs):
         random.seed(2)  # + MPI.rank(MPI.comm_world))  # need to seed random number
-        self.epsilon = epsilon
+        self.Cahn = Cahn
+        self.h_0 = h_0
+        self.wave = wave
         super().__init__(**kwargs)
 
     def eval(self, values, x):
-        ep = float(self.epsilon)
-        if abs(x[0]) < .5:
+        Cahn = float(self.Cahn)
+        h_0 = float(self.h_0)
+        wave = float(self.wave)
+        if abs(x[0]) <= h_0*1.7:
             # random perturbation
-            # dx = np.random.randn(1) * ep
+            # dx = np.random.randn(1) * Cahn
             # sin perturbation
-            # dx = np.sin(x[1] * 30) * ep
-            values[0] = np.tanh(((x[0] + +.2*np.sin(x[1]*20)) * 2) / (ep * np.sqrt(2)))  # phi(0)
-
+            h = h_0 * np.sin(x[1] * 2 * np.pi / wave)
+            #h = np.random.randn(1)
+            values[0] = np.tanh((x[0] + h) / (Cahn * np.sqrt(2)))  # phi(0)
 
         else:
-            values[0] = np.tanh((x[0]) / (ep * np.sqrt(2)))
+            values[0] = np.tanh((x[0]) / (Cahn * np.sqrt(2)))
         values[1] = 0.0  # mu(0)
 
     def value_shape(self):
         return (2,)  # dimension 2 (phi,mu)
 
 
-### Initialise Problem resolution (not used anymore)
-class CahnHilliardEquation(dolfin.NonlinearProblem):
-    """
-    Creates the problem, initiates the residual vector and the Jacobian matrix
-    """
-
-    def __init__(self, a, L):
-        dolfin.NonlinearProblem.__init__(self)
-        self.L = L
-        self.a = a
-
-    def F(self, b, x):
-        dolfin.assemble(self.L, tensor=b)  # computes the residual vector b
-
-    def J(self, A, x):
-        dolfin.assemble(self.a, tensor=A)  # computes the Jacobian matrix A
-
-
 ### Main functions
 def space_phase(mesh):
     """
     Returns the function space for the phase and mu
-    @param mesh: dolfin mesh
-    @return: Function space
+    :param mesh: dolfin mesh
+    :return: Function space
     """
     element_P1 = dolfin.FiniteElement("Lagrange", mesh.ufl_cell(), 1)
     space_ME = dolfin.FunctionSpace(mesh, element_P1 * element_P1)
     return space_ME
 
 
-def initiate_phase(space_ME, epsilon):
+def initiate_phase(space_ME, Cahn, h_0, wave):
     """
     Initiate the phase : from the function space, creates trial functions and test functions, and applies the
     initial conditions
-    @param space_ME: Function space
-    @param epsilon: float, length ratio
-    @return: Functions
+    :param space_ME: Function space
+    :param Cahn: float, Cahn number
+    :param h_0: float, amplitude of the sin
+    :param wave: float, wave number of the sin
+    :return: Functions
     """
     du = dolfin.TrialFunction(space_ME)
     phi_test, mu_test = dolfin.TestFunctions(space_ME)
@@ -78,7 +66,7 @@ def initiate_phase(space_ME, epsilon):
     u = dolfin.Function(space_ME)  # current solution u
     u0 = dolfin.Function(space_ME)  # solution from previous converged step u0
 
-    u_init = InitialConditions(degree=1, epsilon=epsilon)
+    u_init = InitialConditions(degree=1, Cahn=Cahn, h_0=h_0, wave=wave)
     u.interpolate(u_init)
     u0.interpolate(u_init)
 
@@ -88,54 +76,54 @@ def initiate_phase(space_ME, epsilon):
     return phi_test, mu_test, du, u, phi, mu, u0, phi_0, mu_0
 
 
-def problem_phase_with_epsilon(phi_test, mu_test, du, u, phi, mu, phi_0, mu_0, velocity, mid, dt, mob, epsilon, factor):
+def problem_phase_with_epsilon(space_ME, dim_x, dim_y, mesh, phi_test, mu_test, du, u, phi, mu, phi_0, mu_0, velocity,
+                               mid, dt, Pe, Cahn):
     """
     Creates the variational problem
-    @param phi_test: Test function
-    @param mu_test: Test function
-    @param du: Trial function
-    @param u: Function, current solution
-    @param phi: Function, current solution
-    @param mu: Function, current solution
-    @param phi_0: Function, previous solution
-    @param mu_0: Function, previous solution
-    @param velocity: Expression, velocity of the flow for each point of the mesh
-    @param mid: float, for the time discretization
-    @param dt: float, time step
-    @param mob: float, energy factor
-    @param epsilon: float, length ratio
-    @param factor: float, numerical factor 3/2sqrt(2)
-    @return: Functions
+    :param space_ME: function space
+    :param dim_x: dimension in the direction of x
+    :param dim_y: dimension in the direction of y
+    :param mesh: mesh
+    :param phi_test: Test function
+    :param mu_test: Test function
+    :param du: Trial function
+    :param u: Function, current solution
+    :param phi: Function, current solution
+    :param mu: Function, current solution
+    :param phi_0: Function, previous solution
+    :param mu_0: Function, previous solution
+    :param velocity: Expression, velocity of the flow for each point of the mesh
+    :param mid: float, for the time scheme
+    :param dt: float, time step
+    :param Pe: float, Peclet number
+    :param Cahn: float, Cahn number
+    :return: Functions
     """
+    # intermediate mu
     mu_mid = mu_calc(mid, mu, mu_0)
-
+    # define the domain
+    bcs_phase, domain = boundary_conditions_phase(space_ME, dim_x, dim_y, mesh)
+    dx = dolfin.dx(subdomain_data=domain)
+    # variational problem
     L0 = phi * phi_test * dx - phi_0 * phi_test * dx + dt * phi_test * dot(velocity, grad(
-        phi_0)) * dx + dt * factor * epsilon * mob * dot(
-        grad(mu_mid), grad(phi_test)) * dx
-    L1 = mu * mu_test * dx - (phi ** 3 - phi) * (1 / epsilon ** 2) * mu_test * dx - dot(grad(phi), grad(mu_test)) * dx
+        phi_0)) * dx + dt * (1 / Pe) * dot(grad(mu_mid), grad(phi_test)) * dx
+    L1 = mu * mu_test * dx - (phi ** 3 - phi) * mu_test * dx - (Cahn ** 2) * dot(grad(phi), grad(mu_test)) * dx
     F = L0 + L1
 
     J = dolfin.derivative(F, u, du)
 
-    return F, J, u
+    return F, J, u, bcs_phase
 
 
-def solve_phase(F, J, u, space_ME, dim_x, mesh):
+def solve_phase(F, J, u, bcs_phase):
     """
     Solves the variational problem
-    @param F: Function
-    @param J: Function
+    @param F: Function (residual)
+    @param J: Function (Jacobian)
     @param u: Function
+    @param bcs_phase: list, boundary conditions
     @return: Function
-    @param space_ME: function space
-    @param dim_x: dimension in the direction of x
-    @param mesh: mesh
     """
-    # boundaries
-    bcs_phase = boundary_conditions_phase(space_ME, dim_x, mesh)
-    # problem_phase = CahnHilliardEquation(a, L)
-    # solver_phase = dolfin.NewtonSolver()
-
     # Problem
     problem_phase = dolfin.NonlinearVariationalProblem(F, u, bcs_phase, J)
     solver_phase = dolfin.NonlinearVariationalSolver(problem_phase)
@@ -148,50 +136,40 @@ def solve_phase(F, J, u, space_ME, dim_x, mesh):
     prm["newton_solver"]["relaxation_parameter"] = 1.0
     dolfin.parameters["form_compiler"]["optimize"] = True
     dolfin.parameters["form_compiler"]["cpp_optimize"] = True
-    """
-    solver_phase.parameters["linear_solver"] = "lu"
-    solver_phase.parameters["convergence_criterion"] = "incremental"
-    solver_phase.parameters["absolute_tolerance"] = 1e-7
-    solver_phase.parameters["relative_tolerance"] = 1e-4
-    solver_phase.parameters["maximum_iterations"] = 1000
-    """
-
     solver_phase.solve()
     return u
 
 
 ### BOUNDARIES### CREATE BOUNDARIES
-def boundary_conditions_phase(space_ME, dim_x, mesh):
+def boundary_conditions_phase(space_ME, dim_x, dim_y, mesh):
     """
     Creates the boundary conditions  : no slip condition, velocity inflow, pressure out
     :param space_ME: Function space
     :param dim_x: dimensions in the direction of x
+    :param dim_y: dimensions in the direction of y
     :param mesh: mesh
     :return: array of boundary conditions
     """
-    dom_left = BD_left(dim_x)
-    dom_right = BD_right(dim_x)
-    boundaries_phase = dolfin.MeshFunction("size_t", mesh, 1)  # used to define the facets (dimension 1)
-    boundaries_phase.set_all(0)
-    dom_left.mark(boundaries_phase, 1)  # left is marked as (1)
-    dom_right.mark(boundaries_phase, 2)  # right is marked as (2)
-    bc_phi_left = dolfin.DirichletBC(space_ME.sub(0), dolfin.Constant(-1.0), boundaries_phase,
-                                     1)  # phi = -1 on the left (1)
-    bc_phi_right = dolfin.DirichletBC(space_ME.sub(0), dolfin.Constant(1.0), boundaries_phase,
-                                      2)  # phi = +1 on the right (2)
-    bcs_phase = [bc_phi_left, bc_phi_right]
+    domain, boundaries = dom_and_bound(mesh, dim_x, dim_y)
+    # boundary conditions for the phase
+    bc_phi_left = dolfin.DirichletBC(space_ME.sub(0), dolfin.Constant(-1.0), boundaries, 1)  # phi = -1 on the left (1)
+    bc_phi_right = dolfin.DirichletBC(space_ME.sub(0), dolfin.Constant(1.0), boundaries, 2)  # phi = +1 on the right (2)
+    # boundary conditions for mu
+    bc_mu_left = dolfin.DirichletBC(space_ME.sub(1), dolfin.Constant(0.0), boundaries, 1)  # mu = 0 on left and right
+    bc_mu_right = dolfin.DirichletBC(space_ME.sub(1), dolfin.Constant(0.0), boundaries, 2)
+    bcs_phase = [bc_phi_left, bc_phi_right, bc_mu_left, bc_mu_right]
 
-    return bcs_phase
+    return bcs_phase, domain
 
 
 ### Utilitarian functions
 def mu_calc(mid, mu, mu_0):
     """
     Time discretization (Crank Nicholson method)
-    @param mid: float, time discretization
-    @param mu: Function, current solution
-    @param mu_0: Function, previous solution
-    @return: Function
+    :param mid: float, time scheme
+    :param mu: Function, current solution
+    :param mu_0: Function, previous solution
+    :return: Function
     """
     return (1.0 - mid) * mu_0 + mid * mu
 
@@ -221,4 +199,19 @@ def problem_phase_old(phi_test, mu_test, du, u, phi, mu, phi_0, mu_0, velocity, 
 
     a = dolfin.derivative(L, u, du)
     return a, L, u
+    
+### Initialise Problem resolution (not used anymore)
+class CahnHilliardEquation(dolfin.NonlinearProblem):
+
+    def __init__(self, a, L):
+        dolfin.NonlinearProblem.__init__(self)
+        self.L = L
+        self.a = a
+
+    def F(self, b, x):
+        dolfin.assemble(self.L, tensor=b)  # computes the residual vector b
+
+    def J(self, A, x):
+        dolfin.assemble(self.a, tensor=A)  # computes the Jacobian matrix A    
+    
 """
