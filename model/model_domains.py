02a2ddaaad702767d2e5370bf5cd0a02a890a0e0
carogiu <57912591+carogiu@users.noreply.github.com>
2020-02-01 13:09:47 +0000

    UPDATE - Added a function to define the inside domain and the boundaries outside of model_flow and model_phase

--- model/model_domains.py
+++ model/model_domains.py
@@ -43,3 +43,26 @@ class BD_top_bottom(dolfin.SubDomain):
     def inside(self, x, on_boundary):
         d_y = self.dim_y
         return on_boundary and (dolfin.near(x[1], 0, 1E-12) or dolfin.near(x[1], d_y, 1E-12))
+
+
+def dom_and_bound(mesh, dim_x, dim_y):
+    """
+
+    :param mesh: mesh
+    :param dim_x: dimension in the direction of x
+    :param dim_y: dimension in the direction of y
+    :return:
+    """
+    # define interior domain
+    domain = dolfin.MeshFunction("size_t", mesh, 2) # used to define a grid cell (dimension 2)
+    domain.set_all(0)
+    # define the subdomains of the boundaries
+    dom_left = BD_left(dim_x)
+    dom_right = BD_right(dim_x)
+    dom_tb = BD_top_bottom(dim_y)
+    boundaries = dolfin.MeshFunction("size_t", mesh, 1)  # used to define the facets (dimension 1)
+    boundaries.set_all(0)
+    dom_left.mark(boundaries, 1)  # left is marked as (1)
+    dom_right.mark(boundaries, 2)  # right is marked as (2)
+    dom_tb.mark(boundaries, 3)  # tob-bottom is marked as (3)
+    return domain, boundaries
