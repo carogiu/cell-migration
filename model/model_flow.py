a3591372d4887413e70c8905636b5175e85855f8
carogiu <57912591+carogiu@users.noreply.github.com>
2020-02-05 15:46:01 +0000

    UPDATE - Simplify the model for the flow according to the new variational problem (Green-Ostrogradski used only once and not twice)

--- model/model_flow.py
+++ model/model_flow.py
@@ -40,25 +40,15 @@ def problem_coupled(mesh, dim_x, dim_y, w_flow, phi, mu, vi, theta, Ca):
     ds = dolfin.ds(subdomain_data=boundaries)
     # continuous viscosity
     theta_p = theta_phi(theta, phi)
-    # inflow condition
-    v_i = dolfin.Expression(vi, degree=1)  # vi_x for the inflow
-    id_in = dolfin.Expression("exp(-(x[0]+dim_x/2)/(h/2))", degree=1, h=0.05,
-                              dim_x=dim_x)  # = exponential decay for the left boundary
-    # top bottom condition
-    id_tb = dolfin.Expression("exp(-x[1]/(h/2)) + exp((x[1]-dim_y)/(h/2))", degree=1, h=0.05,
-                              dim_y=dim_y)  # = 1 for top bottom, 0 otherwise
-    normal = dolfin.FacetNormal(mesh)
     # Problem
     (velocity, pressure) = dolfin.TrialFunctions(w_flow)
     (v_test, p_test) = dolfin.TestFunctions(w_flow)
-    a_flow = theta_p * dot(velocity, v_test) * dx - pressure * div(v_test) * dx - dot(grad(p_test),
-                                                                                      velocity) * dx + pressure * dot(
-        v_test, normal) * id_tb * ds
-    L_flow = -(1 / Ca) * phi * dot(v_test, grad(mu)) * dx + (id_in * p_test * v_i) * ds
+    a_flow = theta_p * dot(velocity, v_test) * dx + dot(v_test, grad(pressure)) * dx - dot(velocity, grad(p_test)) * dx
+    L_flow = -(1 / Ca) * phi * dot(v_test, grad(mu)) * dx + p_test * ds(1)
     u_flow = dolfin.Function(w_flow)
 
     # v_div, _ = dolfin.split(u_flow)
-    # if abs(div(v_div)) >= 10e-8:  # TODO : Correct calcul of div
+    # if abs(div(v_div)) >= 10e-8:  # TODO : Correct calculation of div
     #    raise ValueError('Divergence should not be higher than a certain threshold - Check Physics solutions')
 
     # Solver
