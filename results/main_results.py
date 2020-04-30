328520fc3ef5b73f9fddc163ff0e234e812743dd
carogiu <57912591+carogiu@users.noreply.github.com>
2020-04-30 12:28:55 +0200

    UPDATE - Add the time of the simulation in the csv files, save vector field for the velocity (in progress)

--- results/main_results.py
+++ results/main_results.py
@@ -11,7 +11,7 @@ from model.model_flow import theta_phi
 
 
 def save_interface_and_peaks(arr_interface: np.ndarray, folder_name: str, time_simu: int, dt: float, h_0: float,
-                             starting_point: float) -> None:
+                             starting_point: float, k_wave: float, h: float) -> None:
     """
 
     :param arr_interface: array, contains the coordinates of the interface (ny x 2)
@@ -20,6 +20,8 @@ def save_interface_and_peaks(arr_interface: np.ndarray, folder_name: str, time_s
     :param dt: float, time step
     :param h_0: float, initial amplitude of the instability
     :param starting_point: float, where the simulation starts
+    :param k_wave: float, wave number of the perturbation
+    :param h: smallest element of the grid
     :return:
     """
     file_name = "results/Figures/" + folder_name + "/interface.csv"
@@ -30,118 +32,34 @@ def save_interface_and_peaks(arr_interface: np.ndarray, folder_name: str, time_s
     # Save the interface coordinates
     len_int = int(a * b)
     interface_one_line = arr_interface.reshape(len_int)
+    interface_with_time = np.asarray([time_simu * dt])
+    interface_with_time = np.concatenate((interface_with_time, interface_one_line))
+
     with open(file_name, 'a') as file:
         writer = csv.writer(file, delimiter=' ')
-        writer.writerow(interface_one_line)
+        writer.writerow(interface_with_time)
 
     # Save the peaks coordinates
-    peaks_t, _ = find_peaks(arr_interface[:, 0])
-    peaks_b, _ = find_peaks(-arr_interface[:, 0])
+    peaks_t, _ = find_peaks(arr_interface[:, 0], distance=(np.pi / (k_wave * h)), height=position_th + 0.8 * h_0)
+    peaks_b, _ = find_peaks(-arr_interface[:, 0], distance=(np.pi / (k_wave * h)), height=0.8 * h_0 - position_th)
+    # peaks_t, _ = find_peaks(arr_interface[:, 0], height=position_th + 0.8 * h_0)
+    # peaks_b, _ = find_peaks(-arr_interface[:, 0], height=0.8*h_0 - position_th)
+
     peaks = np.concatenate((arr_interface[peaks_t, :], arr_interface[peaks_b, :]), axis=0)
     peaks = peaks[peaks[:, 1].argsort()][:, 0]
     amplitude = peaks - position_th
-    amplitude = amplitude[abs(amplitude) >= h_0 / 4]
+    amplitude = amplitude[abs(amplitude) >= h_0 * 0.8]
+    amplitude_with_time = np.asarray([time_simu * dt])
+    amplitude_with_time = np.concatenate((amplitude_with_time, amplitude))
     with open(file_name_final, 'a') as file:
         writer = csv.writer(file, delimiter=' ')
-        writer.writerow(amplitude)
+        writer.writerow(amplitude_with_time)
     return
-
-
-
-def check_div_v(velocity: dolfin.function.function.Function, mesh: dolfin.cpp.generation.RectangleMesh, nx: int,
-                ny: int, dim_x: int, dim_y: int, time: int, folder_name: str) -> None:
-    """
-    Save div(v)
-    :param velocity: dolfin function for the velocity
-    :param mesh: mesh
-    :param nx: grid dimension
-    :param ny: grid dimension
-    :param dim_x: dimension in the direction of x
-    :param dim_y: dimension in the direction of y
-    :param time: time of the simulation
-    :param folder_name: name of the folder where to save the files
-    :return:
-    """
-    fig = plt.figure()
-    p = dolfin.plot(dolfin.div(velocity))
-    p.set_clim(-.5, .5)
-    fig.colorbar(p, boundaries=[-.5, .5], cmap='jet')
-    plt.title('Divergence for t=' + str(time))
-    ax = axes([0, 0, 1, 1], frameon=False)
-    ax.set_axis_off()
-    ax.set_xlim(-dim_x / 2, dim_x / 2)
-    ax.set_ylim(0, dim_y)
-    plt.xlabel('x')
-    plt.ylabel('y')
-    plt.savefig('results/Figures/' + folder_name + "/Checks/Divergence_" + str(time) + '.png')
-    plt.close(fig)
-    """
-    tot_div = np.zeros(1)
-
-    for collection in p.collections:
-        for path in collection.get_paths():
-            #np.append(tot_div, np.asarray(path.to_polygons()))
-            print(path.to_polygons()[0].shape)
-    #print(tot_div)
-    """
-    return
-
-
-def check_hydro(velocity: dolfin.function.function.Function, pressure: dolfin.function.function.Function,
-                u: dolfin.function.function.Function, theta: float, Ca: float,
-                mesh: dolfin.cpp.generation.RectangleMesh, nx: int, ny: int, dim_x: int, dim_y: int, time: int,
-                folder_name: str) -> None:
-    """
-    Check the hydrodynamic relation far from the interface
-    :param velocity: dolfin function for the velocity
-    :param pressure: dolfin function for the pressure
-    :param u: dolfin function for the phase and mu
-    :param theta: viscosity ratio
-    :param Ca: Capillary number
-    :param mesh: mesh
-    :param nx: grid dimension
-    :param ny: grid dimension
-    :param dim_x: dimension in the direction of x
-    :param dim_y: dimension in the direction of y
-    :param time: time of the simulation
-    :param folder_name: name of the folder where to save the files
-    :return:
-    """
-    phi, mu = u.split()
-    theta_p = theta_phi(theta, phi)
-    hydro = dolfin.grad(pressure) + (1 / Ca) * phi * dolfin.grad(mu) + theta_p * velocity
-    fig = plt.figure()
-    plot_hydro_x = dolfin.plot(hydro[0])
-    plot_hydro_x.set_clim(-.1, .1)
-    fig.colorbar(plot_hydro_x, boundaries=[-.1, .1], cmap='jet')
-    plt.title('Hydro_x for t=' + str(time))
-    ax = axes([0, 0, 1, 1], frameon=False)
-    ax.set_axis_off()
-    ax.set_xlim(-dim_x / 2, dim_x / 2)
-    ax.set_ylim(0, dim_y)
-    plt.xlabel('x')
-    plt.ylabel('y')
-    plt.savefig('results/Figures/' + folder_name + "/Checks/Hydro_x_" + str(time) + '.png')
-    plt.close(fig)
-    fig = plt.figure()
-    plot_hydro_y = dolfin.plot(hydro[1])
-    plot_hydro_y.set_clim(-.01, .01)
-    fig.colorbar(plot_hydro_y, boundaries=[-.01, .01], cmap='jet')
-    plt.title('Hydro_y for t=' + str(time))
-    ax = axes([0, 0, 1, 1], frameon=False)
-    ax.set_axis_off()
-    ax.set_xlim(-dim_x / 2, dim_x / 2)
-    ax.set_ylim(0, dim_y)
-    plt.xlabel('x')
-    plt.ylabel('y')
-    plt.savefig('results/Figures/' + folder_name + "/Checks/Hydro_y_" + str(time) + '.png')
-    plt.close(fig)
-    return
-
 
 # To save one array as a heat-map
-def save_fig(arr: np.ndarray, name: str, time_simu: int, dim_x: int, dim_y: int, nx: int, ny: int, theta: float,
-             folder_name: str) -> None or np.ndarray:
+def save_fig(arr: np.ndarray, name: str, time_simu: int, dim_x: float, dim_y: float, nx: int, ny: int, theta: float,
+             folder_name: str, k_wave: float, starting_point: float, dt: float, h_0: float,
+             h: float) -> None or np.ndarray:
     """
     To save one array as a heat-map
     :param arr: array
@@ -160,11 +78,16 @@ def save_fig(arr: np.ndarray, name: str, time_simu: int, dim_x: int, dim_y: int,
         v_min, v_max = -1.1, 1.1
         color_map = 'seismic'
         arr_interface = interface(arr_phi=arr, nx=nx, ny=ny, dim_x=dim_x, dim_y=dim_y)
+        # position_th = time_simu * dt + starting_point
         # plt.plot(arr_interface[:, 0], arr_interface[:, 1], ls=':', c='k')
-        # peaks_t, _ = find_peaks(arr_interface[:, 0])
-        # peaks_b, _ = find_peaks(-arr_interface[:, 0])
-        # plt.plot(arr_interface[peaks_t, 0], arr_interface[peaks_t, 1], "x", c='g')
-        # plt.plot(arr_interface[peaks_b, 0], arr_interface[peaks_b, 1], "x", c='g')
+        # peaks_t, _ = find_peaks(arr_interface[:, 0], distance=(np.pi / (k_wave * h)), height=position_th + 0.8 * h_0)
+        # peaks_b, _ = find_peaks(-arr_interface[:, 0], distance=(np.pi / (k_wave * h)), height=0.8 * h_0 - position_th)
+
+        # peaks_t, _ = find_peaks(arr_interface[:, 0], height=position_th + 0.8 * h_0)
+        # peaks_b, _ = find_peaks(-arr_interface[:, 0], height=0.8*h_0 - position_th)
+
+        plt.plot(arr_interface[peaks_t, 0], arr_interface[peaks_t, 1], "x", c='g')
+        plt.plot(arr_interface[peaks_b, 0], arr_interface[peaks_b, 1], "x", c='k')
     elif name == 'Vy':
         v_min, v_max = -.5, +.5
         color_map = 'jet'
@@ -181,7 +104,7 @@ def save_fig(arr: np.ndarray, name: str, time_simu: int, dim_x: int, dim_y: int,
     plt.xlabel('x')
     plt.ylabel('y')
     file_name = 'results/Figures/' + folder_name + "/" + name + '_' + str(time_simu) + '.png'
-    plt.savefig(fname=file_name)
+    plt.savefig(fname=file_name, dpi=300)
     plt.close(fig)
 
     if name == 'Phase':
@@ -190,8 +113,32 @@ def save_fig(arr: np.ndarray, name: str, time_simu: int, dim_x: int, dim_y: int,
         return
 
 
+def save_quiver(arr_ux: np.ndarray, arr_uy: np.ndarray, time_simu: int, dim_x: float, dim_y: float, nx: int, ny: int,
+                folder_name: str, starting_point, dt):
+    X = np.linspace(-dim_x / 2, dim_x / 2, nx + 1)
+    Y = np.linspace(0, dim_y, ny + 1)
+    X, Y = np.meshgrid(X, Y)
+
+    pos = starting_point + dt * time_simu
+
+    M = np.hypot(arr_ux, arr_uy) * np.sign(arr_ux)
+
+    fig, ax = plt.subplots()
+    q = ax.quiver(X[::20, ::3], Y[::20, ::3], arr_ux[::20, ::3], arr_uy[::20, ::3], M[::20, ::3], pivot='tail')  # [y,x]
+    # ax.scatter(X[::10, ::3], Y[::10, ::3], color='0.5', s=1)
+    plt.plot(np.ones(ny + 1) * pos, np.linspace(0, dim_y, ny + 1), ls=':', c='r')
+    plt.title('Velocity field for t=' + str(time_simu))
+    plt.xlabel('x')
+    plt.ylabel('y')
+    # ax.quiverkey(q, X=0.3, Y=1.1, U=10,label='Quiver key, length = 10', labelpos='E')
+    file_name = 'results/Figures/' + folder_name + "/Velocity_field_" + str(time_simu) + '.png'
+    plt.savefig(fname=file_name, dpi=300)
+    plt.close(fig)
+    return
+
+
 # Extracts the interface from the phase
-def interface(arr_phi: np.ndarray, nx: int, ny: int, dim_x: int, dim_y: int) -> np.ndarray:
+def interface(arr_phi: np.ndarray, nx: int, ny: int, dim_x: float, dim_y: float) -> np.ndarray:
     """
     Find the interface from the phase, where phi=0
     :param arr_phi: values of phi for a given time
@@ -284,6 +231,96 @@ def array_exp_phase(u: dolfin.function.function.Function, mesh: dolfin.cpp.gener
     arr_mu = np.reshape(arr_mu, (ny + 1, nx + 1))
     return arr_phi, arr_mu
 
+
+# def check_div_v(velocity: dolfin.function.function.Function, mesh: dolfin.cpp.generation.RectangleMesh, nx: int,
+#                 ny: int, dim_x: float, dim_y: float, time: int, folder_name: str) -> None:
+#     """
+#     Save div(v)
+#     :param velocity: dolfin function for the velocity
+#     :param mesh: mesh
+#     :param nx: grid dimension
+#     :param ny: grid dimension
+#     :param dim_x: dimension in the direction of x
+#     :param dim_y: dimension in the direction of y
+#     :param time: time of the simulation
+#     :param folder_name: name of the folder where to save the files
+#     :return:
+#     """
+#     fig = plt.figure()
+#     p = dolfin.plot(dolfin.div(velocity))
+#     p.set_clim(-.5, .5)
+#     fig.colorbar(p, boundaries=[-.5, .5], cmap='jet')
+#     plt.title('Divergence for t=' + str(time))
+#     ax = axes([0, 0, 1, 1], frameon=False)
+#     ax.set_axis_off()
+#     ax.set_xlim(-dim_x / 2, dim_x / 2)
+#     ax.set_ylim(0, dim_y)
+#     plt.xlabel('x')
+#     plt.ylabel('y')
+#     plt.savefig('results/Figures/' + folder_name + "/Checks/Divergence_" + str(time) + '.png')
+#     plt.close(fig)
+#     """
+#     tot_div = np.zeros(1)
+#
+#     for collection in p.collections:
+#         for path in collection.get_paths():
+#             #np.append(tot_div, np.asarray(path.to_polygons()))
+#             print(path.to_polygons()[0].shape)
+#     #print(tot_div)
+#     """
+#     return
+#
+#
+# def check_hydro(velocity: dolfin.function.function.Function, pressure: dolfin.function.function.Function,
+#                 u: dolfin.function.function.Function, theta: float, Ca: float,
+#                 mesh: dolfin.cpp.generation.RectangleMesh, nx: int, ny: int, dim_x: float, dim_y: float, time: int,
+#                 folder_name: str) -> None:
+#     """
+#     Check the hydrodynamic relation far from the interface
+#     :param velocity: dolfin function for the velocity
+#     :param pressure: dolfin function for the pressure
+#     :param u: dolfin function for the phase and mu
+#     :param theta: viscosity ratio
+#     :param Ca: Capillary number
+#     :param mesh: mesh
+#     :param nx: grid dimension
+#     :param ny: grid dimension
+#     :param dim_x: dimension in the direction of x
+#     :param dim_y: dimension in the direction of y
+#     :param time: time of the simulation
+#     :param folder_name: name of the folder where to save the files
+#     :return:
+#     """
+#     phi, mu = u.split()
+#     theta_p = theta_phi(theta, phi)
+#     hydro = dolfin.grad(pressure) + (1 / Ca) * phi * dolfin.grad(mu) + theta_p * velocity
+#     fig = plt.figure()
+#     plot_hydro_x = dolfin.plot(hydro[0])
+#     plot_hydro_x.set_clim(-.1, .1)
+#     fig.colorbar(plot_hydro_x, boundaries=[-.1, .1], cmap='jet')
+#     plt.title('Hydro_x for t=' + str(time))
+#     ax = axes([0, 0, 1, 1], frameon=False)
+#     ax.set_axis_off()
+#     ax.set_xlim(-dim_x / 2, dim_x / 2)
+#     ax.set_ylim(0, dim_y)
+#     plt.xlabel('x')
+#     plt.ylabel('y')
+#     plt.savefig('results/Figures/' + folder_name + "/Checks/Hydro_x_" + str(time) + '.png')
+#     plt.close(fig)
+#     fig = plt.figure()
+#     plot_hydro_y = dolfin.plot(hydro[1])
+#     plot_hydro_y.set_clim(-.01, .01)
+#     fig.colorbar(plot_hydro_y, boundaries=[-.01, .01], cmap='jet')
+#     plt.title('Hydro_y for t=' + str(time))
+#     ax = axes([0, 0, 1, 1], frameon=False)
+#     ax.set_axis_off()
+#     ax.set_xlim(-dim_x / 2, dim_x / 2)
+#     ax.set_ylim(0, dim_y)
+#     plt.xlabel('x')
+#     plt.ylabel('y')
+#     plt.savefig('results/Figures/' + folder_name + "/Checks/Hydro_y_" + str(time) + '.png')
+#     plt.close(fig)
+#     return
 
 # Not used anymore
 
