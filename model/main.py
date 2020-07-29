fa7d688da8b6d081d8595d40c7d51277a0e35246
carogiu <57912591+carogiu@users.noreply.github.com>
2020-07-29 19:20:45 +0200

    UPDATE - Add a way to select between Darcy model and Toner-Tu model

--- model/main.py
+++ model/main.py
@@ -3,13 +3,14 @@ import dolfin
 import time
 
 ### Imports
-from model.model_common import mesh_from_dim, time_evolution, time_evolution_with_activity
+from model.model_common import mesh_from_dim, \
+    time_evolution_passive_darcy_2_solvers, time_evolution_active_darcy_2_solvers, \
+    time_evolution_passive_toner_tu_2_solvers, time_evolution_active_tuner_tu_2_solvers
 from model.model_phase import space_phase
 from model.model_flow import space_flow
+from model.unique_solver import space_common, time_evolution_general, time_evolution_toner_tu
 
-from model.unique_solver import space_common, time_evolution_general
-
-### Constants
+### Optimization parameters
 dolfin.parameters["form_compiler"]["optimize"] = True
 dolfin.parameters["form_compiler"]["cpp_optimize"] = True
 
@@ -37,6 +38,7 @@ def main_model(config):
     Pe = config.Pe
     Ca = config.Ca
     starting_point = config.starting_point
+    model = config.model_type
 
     # Initial perturbation parameters
     h_0, k_wave = config.h_0, config.k_wave
@@ -55,21 +57,55 @@ def main_model(config):
 
     # Compute the model
 
-    time_evolution_general(mesh=mesh, dim_x=dim_x, dim_y=dim_y, dt=dt, n=n, mixed_space=mixed_space, theta=theta,
-                           Cahn=Cahn, Pe=Pe, Ca=Ca, starting_point=starting_point, h_0=h_0, k_wave=k_wave, vi=vi,
-                           alpha=alpha, folder_name=folder_name)
+    # One solver
+    # The 2 flow equations and the 2 phase field equations are all solved together
 
-    # if alpha == 0:
-    #    print('Simulation without activity')
-    #    time_evolution(mesh=mesh, dim_x=dim_x, dim_y=dim_y, dt=dt, n=n, space_ME=space_ME, w_flow=w_flow, theta=theta,
-    #                   Cahn=Cahn, Pe=Pe, Ca=Ca, starting_point=starting_point, h_0=h_0, k_wave=k_wave, vi=vi,
-    #                   folder_name=folder_name)
-    # else:
-    #    print('Simulation with activity')
-    #    time_evolution_with_activity(mesh=mesh, dim_x=dim_x, dim_y=dim_y, dt=dt, n=n, space_ME=space_ME, w_flow=w_flow,
-    #                                 theta=theta, alpha=alpha, Cahn=Cahn, Pe=Pe, Ca=Ca, starting_point=starting_point,
-    #                                 h_0=h_0, k_wave=k_wave, vi=vi, folder_name=folder_name)
+    if model == 'darcy':
+        print('Simulating Darcy')
+        time_evolution_general(mesh=mesh, dim_x=dim_x, dim_y=dim_y, dt=dt, n=n, mixed_space=mixed_space, theta=theta,
+                               Cahn=Cahn, Pe=Pe, Ca=Ca, starting_point=starting_point, h_0=h_0, k_wave=k_wave, vi=vi,
+                               alpha=alpha, folder_name=folder_name)
+    else:
+        print('Simulating Toner-Tu')
+        time_evolution_toner_tu(mesh=mesh, dim_x=dim_x, dim_y=dim_y, dt=dt, n=n, mixed_space=mixed_space, theta=theta,
+                                Cahn=Cahn, Pe=Pe, Ca=Ca, starting_point=starting_point, h_0=h_0, k_wave=k_wave, vi=vi,
+                                alpha=alpha, folder_name=folder_name)
 
+    # Two solvers
+    # In that case, the phase field is solved first with the solution of the velocity from the previous time step,
+    # and the flow is then solved with the new values of the phase field.
+    # Less exact, but faster and no difference in the growth rate observed yet
+
+    """
+    if model == 'darcy':
+        print('Simulating Darcy')
+        if alpha == 0:
+            print('Simulation without activity')
+            time_evolution_passive_darcy_2_solvers(mesh=mesh, dim_x=dim_x, dim_y=dim_y, dt=dt, n=n, space_ME=space_ME,
+                                                   w_flow=w_flow, theta=theta, Cahn=Cahn, Pe=Pe, Ca=Ca,
+                                                   starting_point=starting_point, h_0=h_0, k_wave=k_wave, vi=vi,
+                                                   folder_name=folder_name)
+        else:
+            print('Simulation with activity')
+            time_evolution_active_darcy_2_solvers(mesh=mesh, dim_x=dim_x, dim_y=dim_y, dt=dt, n=n, space_ME=space_ME,
+                                                  w_flow=w_flow, theta=theta, alpha=alpha, Cahn=Cahn, Pe=Pe, Ca=Ca,
+                                                  starting_point=starting_point, h_0=h_0, k_wave=k_wave, vi=vi,
+                                                  folder_name=folder_name)
+    else:
+        print('Simulating Toner-Tu')
+        if alpha == 0:
+            print('Simulation without activity')
+            time_evolution_passive_toner_tu_2_solvers(mesh=mesh, dim_x=dim_x, dim_y=dim_y, dt=dt, n=n,
+                                                      space_ME=space_ME, w_flow=w_flow, theta=theta, Cahn=Cahn, Pe=Pe,
+                                                      Ca=Ca, starting_point=starting_point, h_0=h_0, k_wave=k_wave,
+                                                      vi=vi, folder_name=folder_name)
+        else:
+            print('Simulation with activity')
+            time_evolution_active_tuner_tu_2_solvers(mesh=mesh, dim_x=dim_x, dim_y=dim_y, dt=dt, n=n, 
+                                                     space_ME=space_ME,w_flow=w_flow, theta=theta, alpha=alpha, 
+                                                     Cahn=Cahn, Pe=Pe, Ca=Ca,starting_point=starting_point, h_0=h_0, 
+                                                     k_wave=k_wave, vi=vi,folder_name=folder_name)
+    """
     t2 = time.time()
     print('Total computation time = ' + str((t2 - t1) / 60) + ' minutes')
 
